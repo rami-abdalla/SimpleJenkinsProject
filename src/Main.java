8e159de8f201a7512d83e36dc02e027f9d6172ea
rami-abdalla <rami.abdallaa@gmail.com>
2025-02-21 23:09:47 -0500

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello world!");
+
+            Thread.sleep(2000);
+        }
+
+        }
+    }
